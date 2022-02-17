062b3a86333b814ad19771348307eb60dc465b5d
김현두 <bbb6045@naver.com>
2022-02-17 15:16:52 +0900

    like_router++

--- tabom/services/article_service.py
+++ tabom/services/article_service.py
@@ -3,6 +3,9 @@ from django.db.models import Prefetch, QuerySet
 from tabom.models import Article, Like
 
 
+def create_an_article(title: str) -> Article:
+    return Article.objects.create(title=title)
+
 def get_an_article(user_id: int, article_id: int) -> Article:
     return Article.objects.prefetch_related(
         Prefetch("like_set", queryset=Like.objects.filter(user_id=user_id), to_attr="my_likes")
@@ -17,6 +20,7 @@ def get_article_list(user_id: int, offset: int, limit: int) -> QuerySet[Article]
             offset : offset + limit
         ]
     )
+
 
 def delete_an_article(article_id: int) -> None:
     Article.objects.filter(id=article_id).delete()
