70898961ac37b04c21d994067e01a338f487d198
김현두 <bbb6045@naver.com>
2022-02-17 20:18:44 +0900

    like_set() ->>> like_count ariticle 모델에 like갯수를 나타네는 변수명 추가

--- tabom/services/like_service.py
+++ tabom/services/like_service.py
@@ -1,13 +1,21 @@
-from tabom.models import Article, Like, User
+from tabom.models import Article, User
+from tabom.models.like import Like
 
 
 def do_like(user_id: int, article_id: int) -> Like:
-
     User.objects.filter(id=user_id).get()
-    Article.objects.filter(id=article_id).get()
+    article = Article.objects.filter(id=article_id).get()
 
-    return Like.objects.create(user_id=user_id, article_id=article_id)
+    like = Like.objects.create(user_id=user_id, article_id=article_id)
+    article.like_count += 1
+    article.save()
+
+    return like
 
 
 def undo_like(user_id: int, article_id: int) -> None:
-    Like.objects.filter(user_id=user_id, article_id=article_id).delete()
+    deleted_cnt, _ = Like.objects.filter(user_id=user_id, article_id=article_id).delete()
+    if deleted_cnt:
+        article = Article.objects.filter(id=article_id).get()
+        article.like_count -= 1
+        article.save()
